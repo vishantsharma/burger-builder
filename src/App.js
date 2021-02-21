0d093d7ba95556cdc5fa9d96704de66b55fee975
vishantsharma <vishantsharma213@gmail.com>
2021-02-21 21:03:49 +0000

    Add Spinner, Axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, { Component } from 'react';
 import Layout from './HOC/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-    <Layout>
-    <BurgerBuilder/>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
